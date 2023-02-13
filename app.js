c7f908310a022e136d1e7efae7bd6dc47394dc16
Eliakim <eliakimribeiro10@hotmail.com>
2023-02-12 21:07:44 -0300

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+var calculadora = require("./calculadora")
+
+
+
+
+console.log(calculadora.soma(150,10))
+console.log(calculadora.sub(50,20))
+console.log(calculadora.multi(50,3))
+console.log(calculadora.div(200,4))
+
+
+console.log(calculadora.soma(440,4430) - calculadora.sub(4444,4444))
---
+++ calculadora.js
@@ -0,0 +1,19 @@
+function div( a , b){
+    return a / b
+}
+function multi (a , b){
+    return a * b;
+}
+function soma (a , b ){
+    return a + b;
+}
+function sub (a , b){
+    return a - b;
+}
+
+module.exports = {
+    div,
+    multi,
+    soma,
+    sub
+}
